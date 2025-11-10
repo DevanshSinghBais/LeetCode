87cc5ae8b886e5fef9cd5247a204b8664dee8361
DevanshSinghBais <devanshsinghbais111@gmail.com>
2025-11-10 17:52:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (88.75%)

---
+++ solutions/1319-unique-number-of-occurrences/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        unordered_set<int> occurrences;
+        for (auto [num, count] : freq) {
+            // if this frequency already seen, return false
+            if (occurrences.count(count)) return false;
+            occurrences.insert(count);
+        }
+        return true;
+    }
+};
+
