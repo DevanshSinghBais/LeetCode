8cd5ed70afe8f6d5ee6416e47800cd04f25da5a3
DevanshSinghBais <devanshsinghbais111@gmail.com>
2025-12-17 13:13:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.7 MB (81.39%)

---
+++ solutions/2058-concatenation-of-array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n); 
+        int k=0;
+        for(int j=1;j<=2;j++)
+        {
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[k++]=nums[i];
+        }
+        }
+        
+
+        return ans;
+        
+    }
+};
