f77c6aa198adbc121f9ffd551b4c88bca3c0a61c
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-01-20 09:29:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (82.20%)

---
+++ solutions/0231-power-of-two/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        long long sum=0;
+        while(n>0)
+        {
+            sum+=n%2;
+            n=n/2;
+
+        }
+        if(sum==1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+};
