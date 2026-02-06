01604ffbb939d4467297080d665f5eedcc1a613e
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-02-06 16:26:56 +0000

    [LeetCode Sync] Runtime - 14 ms (22.48%), Memory - 7.7 MB (88.72%)

---
+++ solutions/1013-fibonacci-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        return fibonacci(n);
+    }
+private:
+    int fibonacci(int n)
+    {
+        if(n==0||n==1)
+        {
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+};
