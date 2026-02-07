8e2fe23edb59f0adca7c6159abb0249a029512c8
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-02-07 11:33:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.6 MB (98.85%)

---
+++ solutions/0031-next-permutation/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(next_permutation(nums.begin(),nums.end()))
+        {
+            
+        }
+
+        else
+        {
+            sort(nums.begin(),nums.end());
+        }      
+    }
+};
