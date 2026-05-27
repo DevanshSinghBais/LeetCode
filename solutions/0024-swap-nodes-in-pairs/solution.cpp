858b7ddde531e23d49773b69fada381aec9081f0
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-05-27 17:30:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.2 MB (58.45%)

---
+++ solutions/0024-swap-nodes-in-pairs/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL||head->next==NULL)
+        {
+            return head;
+        }
+        ListNode* temp1 = head;
+        ListNode* temp2 = head->next;
+        while(temp2)
+        {
+            int x;
+            x = temp1->val;
+            temp1->val = temp2->val;
+            temp2->val = x;
+            temp1 = temp2->next;
+            if(temp2 && temp2->next)
+{
+    temp2 = temp2->next->next;
+}
+            else
+            {
+                break;
+            }
+        }
+        return head;
+
+    }
+};
