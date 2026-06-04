4014d2b9e47da26d55ec4e24c5f22b4f9e180280
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-06-04 09:10:58 +0000

    [LeetCode Sync] Runtime - 9 ms (86.53%), Memory - 58.1 MB (66.94%)

---
+++ solutions/0148-sort-list/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode* temp = head;
+        vector<int>v;
+        while(temp)
+        {
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        sort(v.begin(),v.end());
+         ListNode* temp1 = head;
+         int m = v.size();
+         int i = 0;
+         while(temp1)
+         {
+            temp1->val = v[i];
+            i++;
+            temp1 = temp1->next;
+         }
+         return head;
+
+    }
+};
