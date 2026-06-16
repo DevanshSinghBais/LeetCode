f6620d7beb1e8262f18e0494ba8af66dae63e993
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-06-16 09:28:39 +0000

    [LeetCode Sync] Runtime - 12 ms (9.10%), Memory - 39.6 MB (93.23%)

---
+++ solutions/3931-process-string-with-special-operations-i/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int m = s.length();
+       
+        string ans;
+        for(int i = 0;i<m;i++)
+        {
+            if(s[i]=='#')
+            {
+                ans += ans;
+            }
+            else if(s[i]=='%')
+            {
+                reverse(ans.begin(),ans.end());
+            }
+            else if(s[i]=='*')
+            {
+                if(!ans.empty())
+                {
+                    ans.pop_back();
+                }
+            }
+            else
+            {
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
