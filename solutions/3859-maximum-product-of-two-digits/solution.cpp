5104cce355f16084a0f274bcfbda30e95d817868
DevanshSinghBais <devanshsinghbais111@gmail.com>
2026-07-25 04:58:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (15.24%)

---
+++ solutions/3859-maximum-product-of-two-digits/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int>v;
+        while(n)
+        {
+            v.push_back(n%10);
+            n = n/10;
+
+        }
+        sort(v.begin(), v.end(), greater<int>());
+        return v[0]*v[1];
+    }
+};
