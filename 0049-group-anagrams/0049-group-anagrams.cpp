0da4a34e9459ea93b22e97651ae79c6fd8448fc2
Devansh Singh Bais <devanshsinghbais111@gmail.com>
2026-09-18 16:25:56 +0530

    Time: 4 ms (99.64%), Space: 20.9 MB (99.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        vector<string> p;
-
-        for (string &s : strs) {
-            p.push_back(s);
-            sort(s.begin(), s.end());
+        unordered_map<string,vector<string>> v;
+        for(int i=0;i<strs.size();i++){
+            string s=strs[i];
+            sort(s.begin(),s.end());
+            v[s].push_back(strs[i]);
         }
-
-        vector<pair<string, int>> v;
-
-        for (int i = 0; i < strs.size(); i++) {
-            v.push_back({strs[i], i});
+        vector<vector<string>>x;
+        for(auto it=v.begin();it!=v.end();it++){
+            x.push_back(it->second);
         }
-
-        sort(v.begin(), v.end());
-
-        ans.push_back({});
-        int q = 0;
-
-        for (int i = 0; i < v.size(); i++) {
-            if (i == 0) {
-                ans[q].push_back(p[v[i].second]);
-            }
-            else {
-                if (v[i].first == v[i - 1].first) {
-                    ans[q].push_back(p[v[i].second]);
-                }
-                else {
-                    q++;
-                    ans.push_back({});
-                    ans[q].push_back(p[v[i].second]);
-                }
-            }
-        }
-
-        return ans;
+        return x;
     }
 };
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
